e3fc550db3eb2be6c2af5994016e112e66973c4b
Mina-Zarif <mina1062016665@gmail.com>
2025-02-26 18:08:23 +0200

    - build resource details view

---
+++ lib/features/post/presentation/widgets/post_comments_view.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:student_portal/core/helpers/app_size_boxes.dart';
+
+import '../../../../core/theming/colors.dart';
+import '../../../../core/widgets/app_text.dart';
+import '../../../home/presentation/widgets/react_bar.dart';
+import '../../../home/presentation/widgets/user_post_view.dart';
+
+class PostCommentsView extends StatelessWidget {
+  const PostCommentsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        buildCommentView(showDivider: true),
+        buildCommentView(),
+      ],
+    );
+  }
+
+  Widget buildCommentView({bool showDivider = false}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        UserPostView(),
+        10.heightBox,
+        IntrinsicHeight(
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Spacer(),
+              if (showDivider) ...[
+                Container(
+                  width: 1,
+                  // height: double.infinity, // This makes it expand to available height
+                  color: ColorsManager.lightGreyColor,
+                ),
+                Spacer(),
+              ],
+              SizedBox(
+                width: 250.w,
+                child: Column(
+                  children: [
+                    AppText(
+                      text:
+                      'These methods include quantitative techniques, such as statistical analysis and machine learning algorithms.',
+                    ),
+                    10.heightBox,
+                    ReactBar(removeShare: true),
+                    10.heightBox,
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
--- lib/features/resource/presentation/pages/resource_details_screen.dart
+++ lib/features/resource/presentation/pages/resource_details_screen.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:student_portal/core/helpers/app_size_boxes.dart';
 
 import '../../../../core/theming/colors.dart';
+import '../../../../core/theming/text_styles.dart';
+import '../../../../core/widgets/custom_appbar.dart';
+import '../../../home/presentation/widgets/post_view.dart';
+import '../../../post/presentation/widgets/comment_bar.dart';
+import '../../../post/presentation/widgets/post_comments_view.dart';
 
-class ResourceDetailsScreen extends StatelessWidget {
+class ResourceDetailsScreen extends StatefulWidget {
   const ResourceDetailsScreen({super.key});
 
   @override
+  State<ResourceDetailsScreen> createState() => _ResourceDetailsScreenState();
+}
+
+class _ResourceDetailsScreenState extends State<ResourceDetailsScreen> {
+  late final ScrollController scrollController;
+
+  @override
+  void initState() {
+    scrollController = ScrollController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
       backgroundColor: ColorsManager.backgroundColor,
-      body: Center(
-        child: Text('data'),
+      appBar: CustomAppBar(
+        backgroundColor: ColorsManager.whiteColor,
+        title: Text('Resource Details', style: Styles.font20w600),
+      ),
+      body: SingleChildScrollView(
+        controller: scrollController,
+        padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 16.h),
+        child: PostView(
+          id: 0,
+          detailsChildren: [
+            CommentBar(),
+            20.heightBox,
+            ...List.generate(2, (index) => PostCommentsView()),
+          ],
+        ),
       ),
     );
   }
