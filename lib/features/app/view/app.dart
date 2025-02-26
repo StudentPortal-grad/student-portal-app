e3b1547666751addad3fb7edb50779895a4d3e7f
Mina-Zarif <mina1062016665@gmail.com>
2025-02-26 17:39:06 +0200

    - add defult theme

--- lib/features/app/view/app.dart
+++ lib/features/app/view/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../core/theming/colors.dart';
 import '../../../core/utils/app_router.dart';
 import '../mange/app_cubit.dart';
 
@@ -20,6 +21,13 @@ class SPApp extends StatelessWidget {
         builder: (context, child) => MaterialApp.router(
           routerConfig: AppRouter.router,
           title: 'Student Portal',
+          themeMode: ThemeMode.light,
+          theme: ThemeData.light().copyWith(
+            scaffoldBackgroundColor: ColorsManager.backgroundColor,
+            primaryColor: ColorsManager.mainColor,
+            primaryColorDark: ColorsManager.mainColorDark,
+            primaryColorLight: ColorsManager.mainColorLight,
+          ),
           debugShowCheckedModeBanner: false,
         ),
       ),
