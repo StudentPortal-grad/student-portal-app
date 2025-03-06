097c1d07e551916b3abe6795bea0fa23377b0204
Mina-Zarif <mina1062016665@gmail.com>
2025-03-06 23:03:21 +0200

    - handle splash screen

--- lib/features/onboarding/view/splash_view/splash_view.dart
+++ lib/features/onboarding/view/splash_view/splash_view.dart
@@ -1,13 +1,15 @@
+import 'dart:async';
 import 'dart:developer';
 
-import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:student_portal/core/helpers/app_size_boxes.dart';
 import '../../../../core/theming/colors.dart';
+import '../../../../core/theming/text_styles.dart';
 import '../../../../core/utils/app_router.dart';
 import '../../../../core/utils/assets_app.dart';
 import '../../../../core/utils/secure_storage.dart';
-import '../../../auth/data/model/token_model/token_model.dart';
+import '../../../../core/widgets/custom_image_view.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -18,6 +20,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   int loadingProgress = 0;
+  Timer? _timer;
 
   @override
   void initState() {
@@ -26,69 +29,55 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   void startLoadingAnimation() {
-    Future.delayed(const Duration(milliseconds: 900), () {
-      setState(() {
-        if (loadingProgress == 3) {
-          navigateToHome();
-        } else {
-          loadingProgress = (loadingProgress + 1) % 4;
-          startLoadingAnimation();
-        }
-      });
+    _timer = Timer.periodic(const Duration(milliseconds: 900), (timer) {
+      if (loadingProgress < 3) {
+        setState(() => loadingProgress++);
+      } else {
+        timer.cancel();
+        navigateToHome();
+      }
     });
   }
 
-  void navigateToHome() async {
-    TokenModel? model = await SecureStorage().readSecureData();
-    // bool isFirst = await SecureStorage().readOnboardingData();
-    log(model?.toJson().toString() ?? "NULL");
-    AppRouter.router.pushReplacement(AppRouter.onBoardingView);
-    return;
-/*
-    if (model?.id == null ||
-        model?.refreshToken == null ||
-        model?.accessToken == null) {
-      AppRouter.router.pushReplacement(AppRouter.signupView);
-      // AppRouter.router.pushReplacement((isFirst) ? AppRouter.onBoardingView : AppRouter.signupView);
-    } else {
-      AppRouter.router.pushReplacement(AppRouter.homeView);
-    }
-    */
+  void navigateToHome() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      final model = await SecureStorage().readSecureData();
+      final isFirst = await SecureStorage().readOnboardingData();
+      log(model?.toJson().toString() ?? "NULL");
+      if (model?.id == null || model?.accessToken == null) {
+        AppRouter.router.pushReplacement(
+          isFirst ? AppRouter.onBoardingView : AppRouter.loginView,
+        );
+      } else {
+        AppRouter.router.pushReplacement(AppRouter.homeView);
+      }
+    });
   }
 
   @override
   void dispose() {
+    _timer?.cancel(); // Ensure the timer is canceled to avoid memory leaks.
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final Size size = MediaQuery.sizeOf(context);
     return Scaffold(
-      backgroundColor: ColorsManager.backgroundColor,
-      body: Padding(
-        padding: EdgeInsetsDirectional.only(
-          top: size.height * 0.48,
-          bottom: 25,
-        ),
-        child: Column(
+      backgroundColor: ColorsManager.mainColor,
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-              child: Image.asset(
-                AssetsApp.logo,
-                fit: BoxFit.cover,
-                width: 125,
-              ),
+            CustomImageView(
+              imagePath: AssetsApp.logo,
+              height: 50.r,
+              width: 50.r,
+              color: Colors.white,
             ),
-            SizedBox(height: size.height * 0.03),
-            DotsIndicator(
-              dotsCount: 4,
-              mainAxisSize: MainAxisSize.min,
-              position: loadingProgress,
-              decorator: const DotsDecorator(
-                color: Color(0xffc9e1b9),
-                activeColor: ColorsManager.mainColor,
-              ),
+            10.widthBox,
+            Text(
+              "Student Portal",
+              style: Styles.font22w700.copyWith(color: Colors.white, fontSize: 28.sp),
             ),
           ],
         ),
